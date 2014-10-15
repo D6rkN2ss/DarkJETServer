678875952b8f30b97a4b808414a6cc50086eed29
Blue Electric <fkdlxmslrx@gmail.com>
2014-10-15 21:41:03 +0900

    Create NetworkManager
    NetworkManager!
    Blue Electric을 직접공격!

--- src/darkjet/server/Leader.java
+++ src/darkjet/server/Leader.java
@@ -1,12 +1,16 @@
 package darkjet.server;
 
+import darkjet.server.network.NetworkManager;
+
 /**
  * Leader of Managers
  * @author Blue Electric
  */
 public final class Leader {
+	public final NetworkManager network;
+	
 	public Leader() {
-		
+		network = new NetworkManager(this);
 	}
 	
 	public static class BaseManager {
---
+++ src/darkjet/server/network/NetworkManager.java
@@ -0,0 +1,11 @@
+package darkjet.server.network;
+
+import darkjet.server.Leader;
+import darkjet.server.Leader.BaseManager;
+
+public final class NetworkManager extends BaseManager {
+	public NetworkManager(Leader leader) {
+		super(leader);
+	}
+
+}
