578279c6d7a6ba96f092df76ae71a910c8a7fc4e
Blue Electric <fkdlxmslrx@gmail.com>
2014-10-17 01:00:36 +0900

    Add MessagePacket & ChatPacket
    서버: 채팅가능!
    클라이언트: 명령어는?
    서버: 시밤�

---
+++ src/darkjet/server/network/minecraft/ChatPacket.java
@@ -0,0 +1,43 @@
+package darkjet.server.network.minecraft;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+
+public final class ChatPacket extends BaseMinecraftPacket {
+	@Override
+	public int getPID() {
+		return MinecraftIDs.CHAT;
+	}
+	
+	public String sender;
+	public String message;
+	
+	public ChatPacket(String sender, String message) {
+		this.sender = sender;
+		this.message = message;
+	}
+
+	@Override
+	public byte[] getResponse() {
+		int totalLength = 6+sender.length() + message.length();
+		bb = ByteBuffer.allocate(totalLength);
+		short senderLength = (short) sender.length();
+		short messageLength = (short) message.length();
+		bb.put( MinecraftIDs.CHAT );
+		bb.putShort(senderLength);
+		try{
+			bb.put(sender.getBytes("UTF-8"));
+			bb.putShort(messageLength);
+			bb.put(message.getBytes(Charset.forName("UTF-8")));
+		}
+		catch(UnsupportedEncodingException e){
+			e.printStackTrace();
+			throw new RuntimeException("UTF-8 Not Support!?");
+		}
+		return bb.array();
+	}
+	
+	
+
+}
