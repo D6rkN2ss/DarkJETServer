b5501cd7f3d1c5aecd175a014a2806c30406ffcf
Blue Electric <fkdlxmslrx@gmail.com>
2014-10-16 20:34:06 +0900

    Add SetHealthPacket
    (체력이) 변-해라!

---
+++ src/darkjet/server/network/minecraft/SetHealthPacket.java
@@ -0,0 +1,24 @@
+package darkjet.server.network.minecraft;
+
+import java.nio.ByteBuffer;
+
+public final class SetHealthPacket extends BaseMinecraftPacket {
+	@Override
+	public int getPID() {
+		return MinecraftIDs.SET_HEALTH;
+	}
+	public byte health;
+	
+	public SetHealthPacket(byte health) {
+		this.health = health;
+	}
+
+	@Override
+	public byte[] getResponse() {
+		bb = ByteBuffer.allocate( 2);
+		bb.put( MinecraftIDs.SET_HEALTH );
+		bb.put( (byte) health );
+		
+		return bb.array();
+	}
+}
