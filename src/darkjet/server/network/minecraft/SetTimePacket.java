4af4fc1859d1cb81db72e0fd1e679a532f42f25d
Blue Electric <fkdlxmslrx@gmail.com>
2014-10-16 20:26:54 +0900

    Add SetTimePacket
    (시간아) 변-해라!

---
+++ src/darkjet/server/network/minecraft/SetTimePacket.java
@@ -0,0 +1,27 @@
+package darkjet.server.network.minecraft;
+
+import java.nio.ByteBuffer;
+
+public final class SetTimePacket extends BaseMinecraftPacket {
+	@Override
+	public int getPID() {
+		return MinecraftIDs.SET_TIME;
+	}
+	public int time;
+	
+	public SetTimePacket(int time) {
+		this.time = time;
+	}
+
+	@Override
+	public byte[] getResponse() {
+		bb = ByteBuffer.allocate( 1 + Integer.BYTES + 1 );
+		bb.put( MinecraftIDs.SET_TIME );
+		bb.putInt( time );
+		//TODO 0x00
+		bb.put( (byte) 0x80 ); //true
+		
+		return bb.array();
+	}
+	
+}
