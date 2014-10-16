21d7ee22bf3ff73d9e9ff09b1bf7b5cf73508e2a
Blue Electric <fkdlxmslrx@gmail.com>
2014-10-16 18:47:56 +0900

    Add Vector

---
+++ src/darkjet/server/math/Vector.java
@@ -0,0 +1,37 @@
+package darkjet.server.math;
+
+public final class Vector {
+	private int x, y, z;
+
+	public Vector(int v){
+		this(v, v, v);
+	}
+	public Vector(int x, int y){
+		this(x, y, 0);
+	}
+	public Vector(int x, int y, int z){
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+	public int getX(){
+		return x;
+	}
+	public int getY(){
+		return y;
+	}
+	public int getZ(){
+		return z;
+	}
+
+	public void setX(int v){
+		this.x = v;
+	}
+	public void setY(int v){
+		this.y = v;
+	}
+	public void setZ(int v){
+		this.z = v;
+	}
+}
