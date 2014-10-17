f58c22c16a5eab3179824e595eebf40a75d9dc4c
Blue Electric <fkdlxmslrx@gmail.com>
2014-10-17 22:53:04 +0900

    Add MovePlayerPacket
    이동! 돌진하라

---
+++ src/darkjet/server/network/minecraft/MovePlayerPacket.java
@@ -0,0 +1,50 @@
+package darkjet.server.network.minecraft;
+
+import java.nio.ByteBuffer;
+
+public final class MovePlayerPacket extends BaseMinecraftPacket {
+	public int eid;
+	public float x;
+	public float y;
+	public float z;
+	public float yaw;
+	public float pitch;
+	public float bodyYaw;
+	public boolean isTeleport = false;
+	
+	@Override
+	public int getPID() {
+		return MinecraftIDs.MOVE_PLAYER;
+	}
+
+	@Override
+	public void parse() {
+		bb.get();
+		eid = bb.getInt();
+		x = bb.getFloat();
+		y = bb.getFloat();
+		z = bb.getFloat();
+		yaw = bb.getFloat();
+		pitch = bb.getFloat();
+		bodyYaw = bb.getFloat();
+		isTeleport = ((bb.get() & 0x80) > 0);
+	}
+
+	@Override
+	public byte[] getResponse() {
+		bb = ByteBuffer.allocate( 2 + Integer.BYTES + (Float.BYTES * 6) );
+		bb.put(MinecraftIDs.MOVE_PLAYER);
+		bb.putInt(eid);
+		bb.putFloat(x);
+		bb.putFloat(y);
+		bb.putFloat(z);
+		bb.putFloat(yaw);
+		bb.putFloat(pitch);
+		bb.putFloat(bodyYaw);
+		bb.put((byte) (isTeleport == true ? 0x80 : 0x00));
+		
+		return bb.array();
+	}
+	
+	
+}
