17df713839807383676e24a624ee2414e7ead1d5
Blue Electric <fkdlxmslrx@gmail.com>
2014-10-17 17:29:14 +0900

    Add Vector2
    Chunk.Pos+it=ion!(V×e÷c=t.o±r∽2!)
    
    이건 좀 무리수였나

---
+++ src/darkjet/server/math/Vector2.java
@@ -0,0 +1,27 @@
+package darkjet.server.math;
+
+public final class Vector2 {
+	private int x, z;
+
+	public Vector2(int v){
+		this(v, v);
+	}
+	public Vector2(int x, int z){
+		this.x = x;
+		this.z = z;
+	}
+
+	public int getX(){
+		return x;
+	}
+	public int getZ(){
+		return z;
+	}
+
+	public void setX(int v){
+		this.x = v;
+	}
+	public void setZ(int v){
+		this.z = v;
+	}
+}
