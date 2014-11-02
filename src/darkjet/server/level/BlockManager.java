a266ccd100df2c3f5f45ea593a93b19ad07e0458
Blue Electric <fkdlxmslrx@gmail.com>
2014-11-02 14:35:05 +0900

    Add onUpdate to Block

--- src/darkjet/server/level/BlockManager.java
+++ src/darkjet/server/level/BlockManager.java
@@ -1,20 +1,36 @@
 package darkjet.server.level;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 
+import darkjet.server.Leader;
 import darkjet.server.block.Block;
 import darkjet.server.math.Vector;
+import darkjet.server.tasker.MethodTask;
 
 /**
  * Block Update Manager for Level
  * @author Blue Electric
  */
 public final class BlockManager {
-	public final ArrayList<Vector> updateList = new ArrayList<>();
+	public final LinkedList<Vector> updateList = new LinkedList<>();
 	public final Level level;
 	
-	public BlockManager(Level level) {
+	public BlockManager(Leader leader, Level level) {
 		this.level = level;
+		
+		try {
+			leader.task.addTask( new MethodTask(-1, 5, this, "onUpdate") );
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public final void onUpdate(long currentTick) throws Exception {
+		while ( !updateList.isEmpty() ) {
+			Vector v = updateList.poll();
+			Block b = Block.getBlock( level.getBlock(v) );
+			b.onUpdate(level);
+		}
 	}
 	
 	public final void onChange(int x, int y, int z, byte id, byte meta) throws Exception {
